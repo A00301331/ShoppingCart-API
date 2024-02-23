4bd55118f3f688c1e2c0688dbd25229d8f8d3137
A00301331 <160158614+A00301331@users.noreply.github.com>
2024-02-23 19:46:46 +0000

    Second Commit

---
+++ AuthAPIModels/Category.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace AuthAPIModels
+{
+    public class Category
+    {
+        public int id_ { get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ AuthAPIModels/Product.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AuthAPIModels
+{
+    public class Product
+    {
+        public int id_ { get; set; }
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public string description { get; set; }
+    }
+}
